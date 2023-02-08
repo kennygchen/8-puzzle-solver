7fc5ddd75d7d7c35c43eb74369b9f59ba534fc78
Kenny Chen <kennygchen@yahoo.com>
2023-02-08 00:53:22 -0800

    euclideanDistance

--- h_n.py
+++ h_n.py
@@ -1,14 +1,21 @@
+from math import *
+
 def misplacedTiles(current_state, goal_state):
     count = 0
     for i in range(len(current_state)):
         for j in range(len(current_state[0])):
             if ((current_state[i][j]) and current_state[i][j] != goal_state[i][j]):
                 count += 1
-    return count - 1
+    return count
 
 def euclideanDistance(current_state, goal_state):
     total = 0
     for i in range(len(current_state)):
         for j in range(len(current_state[0])):
-           pass 
+           if ((current_state[i][j]) and current_state[i][j] != goal_state[i][j]):
+            for x in range(len(current_state)):
+                for y in range(len(current_state[0])):
+                    if (goal_state[x][y] == current_state[i][j]):
+                        distance = sqrt(pow(i - j, 2) + pow(x - y, 2))
+                        total += distance
     return total
